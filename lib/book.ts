30fce741f543562ee8c0563aa4c2e2ad314e74b5
ThangPhan <thang.phan@kinto-mobility.biz>
2021-08-02 16:17:10 +0900

    fix flicket

--- lib/book.ts
+++ lib/book.ts
@@ -1,88 +1,90 @@
-import { bundleMDX } from 'mdx-bundler'
-import type { LangOptions } from '../constants/lang'
-import { Lang } from '../constants/lang'
+import {bundleMDX} from 'mdx-bundler'
+import type {LangOptions} from '../constants/lang'
+import {Lang} from '../constants/lang'
 import path from 'path'
 import fs from 'fs'
 
 const blogsDirectory = path.join(process.cwd(), 'contents')
 type PathsBlog = Array<{
-	params: {
-		book: string;
-		id: string
-	}
+  params: {
+    book: string
+    id: string
+  }
 }>
 
 function getAllBlogIds() {
-	const pathsBlog: PathsBlog = []
-	const blogNames = fs
-		.readdirSync(blogsDirectory)
-		.map((blogName: string) => blogName)
+  const pathsBlog: PathsBlog = []
+  const blogNames = fs
+    .readdirSync(blogsDirectory)
+    .map((blogName: string) => blogName)
 
-	blogNames.forEach((blogName: string) => {
-		const pathBlog = fs.readdirSync(`${blogsDirectory}/${blogName}`).map((f: string) => ({
-			params: { book: blogName, id: f.replace('.mdx', '') },
-		}))
+  blogNames.forEach((blogName: string) => {
+    const pathBlog = fs
+      .readdirSync(`${blogsDirectory}/${blogName}`)
+      .map((f: string) => ({
+        params: {book: blogName, id: f.replace('.mdx', '')},
+      }))
 
-		pathsBlog.push(...pathBlog)
-	})
-	return pathsBlog
+    pathsBlog.push(...pathBlog)
+  })
+  return pathsBlog
 }
 
 async function getBlogData(book: string, id: string) {
-	const { code, frontmatter } = await getFrontmatter(`${book}/${id}.mdx`)
-	return {
-		code,
-		frontmatter,
-	}
+  const {code, frontmatter} = await getFrontmatter(`${book}/${id}.mdx`)
+  return {
+    code,
+    frontmatter,
+  }
 }
-
 
 async function getAllBlogsData(lang: LangOptions) {
-	const blogNames = fs.readdirSync(blogsDirectory)
-	return await Promise.all(
-		blogNames.map(async (blogName) => {
-			const blogFiles = fs.readdirSync(`${blogsDirectory}/${blogName}`).map((f: string) => f)
-			const blogFile = lang === Lang.en ? blogFiles[0] : blogFiles[1]
-			return (await getFrontmatter(`${blogName}/${blogFile}`)).frontmatter
-		}),
-	)
+  const blogNames = fs.readdirSync(blogsDirectory)
+  return await Promise.all(
+    blogNames.map(async (blogName) => {
+      const blogFiles = fs
+        .readdirSync(`${blogsDirectory}/${blogName}`)
+        .map((f: string) => f)
+      const blogFile = lang === Lang.en ? blogFiles[0] : blogFiles[1]
+      return (await getFrontmatter(`${blogName}/${blogFile}`)).frontmatter
+    }),
+  )
 }
 
-
 async function getFrontmatter(blogName: string) {
-	if (process.platform === 'win32') {
-		process.env.ESBUILD_BINARY_PATH = path.join(
-			process.cwd(),
-			'node_modules',
-			'esbuild',
-			'esbuild.exe',
-		)
-	} else {
-		process.env.ESBUILD_BINARY_PATH = path.join(
-			process.cwd(),
-			'node_modules',
-			'esbuild',
-			'bin',
-			'esbuild',
-		)
-	}
+  if (process.platform === 'win32') {
+    process.env.ESBUILD_BINARY_PATH = path.join(
+      process.cwd(),
+      'node_modules',
+      'esbuild',
+      'esbuild.exe',
+    )
+  } else {
+    process.env.ESBUILD_BINARY_PATH = path.join(
+      process.cwd(),
+      'node_modules',
+      'esbuild',
+      'bin',
+      'esbuild',
+    )
+  }
 
-	const blogPath = path.join(
-		blogsDirectory,
-		blogName,
-	)
-	const content = fs.readFileSync(blogPath, 'utf-8')
-	return await bundleMDX(content, {
-		cwd: `${process.cwd()}/public`,
-	})
+  const blogPath = path.join(blogsDirectory, blogName)
+  const content = fs.readFileSync(blogPath, 'utf-8')
+  return await bundleMDX(content, {
+    cwd: `${process.cwd()}/public`,
+  })
 }
 
 async function getOppositeId(bookId: string, id: string) {
-	const blogNames = fs.readdirSync(blogsDirectory)
-	const blogName = blogNames[parseInt(bookId)]
-	const blogFiles = fs.readdirSync(`${blogsDirectory}/${blogName}`).map((f: string) => f).find((f: string) => f !== id)
+  const blogNames = fs.readdirSync(blogsDirectory)
+  const blogName = blogNames[parseInt(bookId)]
+  const blogFiles = fs
+    .readdirSync(`${blogsDirectory}/${blogName}`)
+    .map((f: string) => f)
+    .find((f: string) => f !== id)
 
-	return blogFiles
+  return blogFiles
 }
 
-export { getAllBlogIds, getBlogData, getAllBlogsData, getOppositeId }
+export {getAllBlogIds, getBlogData, getAllBlogsData, getOppositeId}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document'
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -15,7 +14,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script dangerouslySetInnerHTML={{__html: codeToRunOnClient}} />
+        </Head>
         <body className="dark:bg-gray-800">
           <Main />
           <NextScript />
@@ -26,3 +27,27 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+
+function loadColorMode() {
+  function getInitialColorMode() {
+    const theme = window.localStorage.getItem('theme')
+
+    if (typeof theme == 'string') {
+      return theme
+    }
+
+    const mql = window.matchMedia('(prefers-color-scheme: dark)')
+    const hasMediaQueryPreference = typeof mql.matches === 'boolean'
+
+    if (hasMediaQueryPreference) {
+      return mql.matches ? 'dark' : 'light'
+    }
+    return 'light'
+  }
+  const colorMode = getInitialColorMode()
+  const root = document.documentElement
+  root.classList.add(colorMode)
+}
+
+const stringFnc = String(loadColorMode)
+const codeToRunOnClient = `(${stringFnc})()`
