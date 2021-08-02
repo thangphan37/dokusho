58f25bf713a3ad37be111b2f4e5e971280102903
ThangPhan <thang.phan@kinto-mobility.biz>
2021-08-02 14:48:18 +0900

    add cloudary

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/dokusho',
+  },
   async redirects() {
     return [
       {
